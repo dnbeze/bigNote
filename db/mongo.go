ac4cdb1e9e74f37e6b24ae6d913d91149b63bf3c
Brett Ezell <dnbeze55@gmail.com>
2024-03-24 17:18:56 -0400

    updated little error handling

--- db/mongo.go
+++ db/mongo.go
@@ -12,17 +12,17 @@ import (
 func InitDB() (*mongo.Database, error) {
 	MongoURI, err := utils.GetEnv("MONGO_URI")
 	if err != nil {
-		log.Fatalf("failed to load MONGOURI")
+		log.Fatalf("failed to load MONGOURI %v", err)
 	}
 	MongoDB, err := utils.GetEnv("MONGO_DB")
 	if err != nil {
-		log.Fatalf("failed to load MONGOURI")
+		log.Fatalf("failed to load MONGOURI %v", err)
 	}
 	opts := options.Client().ApplyURI(MongoURI)
 
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to connect to mongo %v", err)
 	}
 
 	err = client.Ping(context.TODO(), nil)
